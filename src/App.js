764b292d49b96bf2bbb876a5748c867cbdf55123
Adesh S <adesh.sonawane@morfsys.com>
2021-11-26 08:19:06 +0530

    test 188

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Prenavbar from './components/Prenavbar';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div>
+        <Prenavbar />
+        <Navbar />
+    </div>
+  );
+}
+
+export default App;
