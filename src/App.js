fe77a4bf0c99d3b128e645465876c73793e22895
Adesh S <adesh.sonawane@morfsys.com>
2021-11-26 16:58:30 +0530

    StarProduct added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router} from 'react-router-dom';
 import Slider from './components/Slider.js'
 import data from './Data/data.json'
 import Offers from './components/Offers.js'
+import Heading from './components/Heading.js'
+import StarProduct from './components/StarPrdoduct.js'
+
 
 
 
@@ -13,8 +16,11 @@ function App() {
     <Router>
         <Prenavbar />
         <Navbar />
-        <Slider  start={data.banner.start}/>
-        <Offers offer={data.offer}/>    
+        <Slider   start={data.banner.start}/>
+        <Offers   offer={data.offer}/>   
+        <Heading  text="STAR PRODUCTS"/> 
+        <StarProduct  starProduct={data.starProduct}/>
+        <Heading  text="HOT ACCESSORIES"/> 
     </Router>    
   );
 }
